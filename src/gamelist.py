241c834fc9eeaaf6e04c88836470535c8e9ac664
Redlavaa <98986258+Redlavaa@users.noreply.github.com>
2026-02-03 21:12:04 -0600

    added stuff

---
+++ src/gamelist.py
@@ -0,0 +1,47 @@
+from playwright.async_api import async_playwright
+import json
+import time
+import discord
+
+async def list():
+    async with async_playwright() as p:
+        start_time = time.perf_counter()
+        browser = await p.chromium.connect_over_cdp("http://localhost:9222")
+        context = browser.contexts[0]
+        page = await context.new_page()
+
+        try:
+            await page.goto(f"https://polytoria.com/api/places", wait_until="domcontentloaded", timeout=1000000)
+
+            raw_text = await page.inner_text("body")
+            data = json.loads(raw_text)
+
+            names = []
+            ids = []
+
+            top = data["data"][:5]
+
+            for item in top:
+                names.append(item["name"])
+                ids.append(str(item["id"]))
+
+            end_time = time.perf_counter()
+            execution_time = round((end_time - start_time) * 1000, 2)
+
+            embed = discord.Embed(
+            title="Game List",
+            color=discord.Color.blue()
+        )
+            
+            for i in range(len(names)):
+                embed.add_field(
+                    name=f"{names[i]}",
+                    value=f"[{names[i]}](https://polytoria.com/places/{ids[i]})",
+                    inline=False
+                )
+                embed.set_footer(text=f"Execution Time: {execution_time} ms")
+
+            return(names, ids, execution_time, embed)
+
+        finally:
+            await page.close()
---
+++ src/main.py
@@ -0,0 +1,58 @@
+import discord
+from discord.ext import commands
+from gamelist import list
+from getplayer import get
+
+TOKEN = "MTQ2Nzk5OTA1ODkzMjI3MzE2NA.GZWU91.-Hmj9EJ7cs6GpmfgnJW2RNE_Rcw-5c7Hebs0uY"
+GUILD_ID = 1468373912931926142
+
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.tree.command(name="gamelist", description="Replies with gamelist")
+async def gamelist(interaction: discord.Interaction):
+
+    await interaction.response.defer()
+
+    try:
+        name, id, execution_time, embed = await list()
+
+        await interaction.followup.send(embed=embed)
+    except Exception as e:
+        await interaction.followup.send(f"An error occurred: {type(e).__name__} - {e}")
+
+@bot.tree.command(name="profile", description="Replies with profile info")
+async def profile(interaction: discord.Interaction, id: str):
+
+    await interaction.response.defer()
+
+    try:
+        name, networth, icon, execution_time, embed = await get(id)
+
+        await interaction.followup.send(embed=embed)
+    except Exception as e:
+        await interaction.followup.send(f"An error occurred: {type(e).__name__} - {e}")
+
+@bot.tree.command(name="getid", description="Replies with your user id")
+async def getid(interaction: discord.Interaction, username: str):
+
+    await interaction.response.defer()
+
+    try:
+        id, execution_time = await getid(username)
+
+        await interaction.followup.send(f"ID: {id}, Execution Time: {execution_time}ms")
+    except Exception as e:
+        await interaction.followup.send(f"An error occurred: {type(e).__name__} - {e}")
+
+@bot.event
+async def on_ready():
+    print(f"Logged in as {bot.user} (ID: {bot.user.id})")
+    try:
+        await bot.tree.sync()
+        print("Slash commands synced")
+    except Exception as e:
+        print("Sync failed:", type(e).__name__, e)
+
+bot.run(TOKEN)
