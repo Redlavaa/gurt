3914690df6f2af0c275b418020e95a8e418a9745
Redlavaa <98986258+Redlavaa@users.noreply.github.com>
2026-03-19 18:27:35 -0500

    changed status again

--- src/main.py
+++ src/main.py
@@ -79,7 +79,7 @@ async def on_ready():
     try:
         await bot.tree.sync()
         print("Slash commands synced")
-        await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="for commands")) # don't include a help command as discord already shows the list when you do /
+        await bot.change_presence(activity=discord.Game(name="waiting for a command"))
     except Exception as e:
         print("Sync failed:", type(e).__name__, e)
 
